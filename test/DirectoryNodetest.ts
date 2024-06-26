7189d8645f7c6fa8a6b0943c01b40712e0c8d028
Martha Ligia De Luque Aaron <martha.deluque@lever.co>
2024-06-26 12:05:23 -0500

    ENDPOINT Backend Coding Challenge - New tests added

--- test/DirectoryNodetest.ts
+++ test/DirectoryNodetest.ts
@@ -1,38 +1,16 @@
-import { describe, it,  } from "node:test";
+import { describe, it } from "node:test";
 import { Directory } from "../src/Directory";
 import { DirectoryNode } from "../src/DirectoryNode";
-
 var assert = require('assert');
 
 describe('DirectoryNode', () => {
-    it('Test create', () => {
+    it('Test sort', () => {
         const directory = new Directory(new DirectoryNode('root'))
-        const nameChild = "testchild"
-        directory.create([nameChild])
-        assert.equal(directory.root.children[0].key, nameChild);
-    });
-    it('Test delete', () => {
-        const directory = new Directory(new DirectoryNode('root'))
-        const nameChild = "testchild"
-        directory.create([nameChild])
-        assert.strictEqual(directory.root.children[0].key, nameChild);
-        directory.delete([nameChild])
-        assert.equal(typeof directory.root.children[0], 'undefined');
-    });
-    it.skip('Test move', () => {
-        const directory = new Directory(new DirectoryNode('root'))
+        const nameChild2 = "testchild2"
+        directory.create([nameChild2])
         const nameChild1 = "testchild1"
         directory.create([nameChild1])
+        directory.root.sortChildren(directory.root.children)
         assert.equal(directory.root.children[0].key, nameChild1);
-        const nameChild2 = "testchild2/asd"
-        directory.create([nameChild2])
-        assert.equal(directory.root.children[1].key, nameChild2);
-        directory.move([nameChild2, nameChild1])
-        assert.equal(directory.root.children[1].children[0], 'asd');
-        const nameChild3 = "testchild3"
-        directory.create([nameChild3])
-        assert.equal(directory.root.children[2].key, nameChild3);
-        directory.move([nameChild1, nameChild3])
-        assert.equal(directory.root.children[2].children[0].key, nameChild1);
     });
-});
+})
